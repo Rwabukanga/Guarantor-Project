48dd7525c1321f685bb484d7867e53442566a557
Rwabukanga <43671798+Rwabukanga@users.noreply.github.com>
2024-03-11 17:22:27 +0200

    removed Corporate attributes

--- Guarantor Procedure.sql
+++ Guarantor Procedure.sql
@@ -9,18 +9,18 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROCEDURE [dbo].[GuarantorLoans]
+Alter PROCEDURE [dbo].[GuarantorLoans]
 	
 AS
 BEGIN
 	
 	SET NOCOUNT ON;
-with ConsumerLoansGuarantor(accountNumber,companyRegNo,companyRegistrationDate,country,dateOfBirth,foreName1,foreName2,foreName3,guarantorType,homeTelephone,institutionName,
-mobileTelephone,nationalId,nationality,passportNumber,placeOfBirth,postalAddressNumber,postalCode,surName,telephone1,telephone2,telephone3,town,tradingName,workTelephone,recordType) as (
+with ConsumerLoansGuarantor(accountNumber,country,dateOfBirth,foreName1,foreName2,foreName3,homeTelephone,
+mobileTelephone,nationalId,nationality,passportNumber,placeOfBirth,postalAddressNumber,postalCode,surName,town,workTelephone,recordType) as (
 
 
 select
-'90'+'-'+convert(nvarchar, TLn04IntGntor.ClientId)+'-'+convert(nvarchar, TLn04IntGntor.LoanId) AS accountNumber,'' as companyRegNo,'' as companyRegistrationDate,'RWANDA' AS country,
+'90'+'-'+convert(nvarchar, TLn04IntGntor.ClientId)+'-'+convert(nvarchar, TLn04IntGntor.LoanId) AS accountNumber,'RWANDA' AS country,
 CASE
     WHEN DATEDIFF(Year,dbo.TCl01Client.DateBirth,GETDATE()) BETWEEN 18 AND 100 THEN  CONVERT(char(8), dbo.TCl01Client.DateBirth ,112) 
 	ELSE '19900101'
@@ -61,24 +61,18 @@ END AS dateOfBirth,
 	charindex( ' ', substring( concat(case when LastName<>'' then LastName else ' ' end,' ',case when FirstName<>'' then FirstName else '_' end,' ',' ',' ',' ',' ',' ',' ',' ',' '), charindex( ' ', concat(case when LastName<>'' then LastName else ' ' end,' ',case when FirstName<>'' then FirstName else '_' end,' ',' ',' ',' ',' ',' ',' ',' ',' ') ) + 1, len( concat(case when LastName<>'' then LastName else ' ' end,' ',case when FirstName<>'' then FirstName else '_' end,' ',' ',' ',' ',' ',' ',' ',' ',' ') ) ) ) + 1, 
 	len( substring( concat(case when LastName<>'' then LastName else ' ' end,' ',case when FirstName<>'' then FirstName else '_' end,' ',' ',' ',' ',' ',' ',' ',' ',' '), charindex( ' ', concat(case when LastName<>'' then LastName else ' ' end,' ',case when FirstName<>'' then FirstName else '_' end,' ',' ',' ',' ',' ',' ',' ',' ',' ') ) + 1, len( concat(case when LastName<>'' then LastName else ' ' end,' ',case when FirstName<>'' then FirstName else '_' end,' ',' ',' ',' ',' ',' ',' ',' ',' ') ) ) ) ) ) ) foreName3,
 
-'' AS guarantorType,
+
 CASE 
 	WHEN LEN(dbo.TCl01Client.MobPhone1)=12 THEN dbo.TCl01Client.MobPhone1 
 	WHEN LEN(dbo.TCl01Client.MobPhone1)=10 THEN '25'+dbo.TCl01Client.MobPhone1
 	WHEN LEN(dbo.TCl01Client.MobPhone1)=9 THEN '250'+dbo.TCl01Client.MobPhone1
 	ELSE '250788888888' 
-END AS mobileTelephone,'' as homeTelephone,'' AS institutionName,
+END AS mobileTelephone,'' as homeTelephone,
 CASE
     WHEN LEN(dbo.TCl01Client.NatIDNber)=16 THEN dbo.TCl01Client.NatIDNber
 	ELSE '1199011111111111'
-END AS nationalId,dbo.TCl550Country.Descript as nationality,' ' AS passportNumber,' ' AS placeOfBirth,'' AS postalAddressNumber,' ' AS postalCode,
-CASE 
-	WHEN LEN(dbo.TCl01Client.MobPhone1)=12 THEN dbo.TCl01Client.MobPhone1 
-	WHEN LEN(dbo.TCl01Client.MobPhone1)=10 THEN '25'+dbo.TCl01Client.MobPhone1
-	WHEN LEN(dbo.TCl01Client.MobPhone1)=9 THEN '250'+dbo.TCl01Client.MobPhone1
-	ELSE '250788888888' 
-END  AS telephone1,
-'' AS telephone2, '' AS telephone3, '' AS town,'' AS tradingName, '' AS workTelephone,'GI' AS recordType
+END AS nationalId,dbo.TCl550Country.Descript as nationality,' ' AS passportNumber,' ' AS placeOfBirth,'' AS postalAddressNumber,' ' AS postalCode,'' as Town,
+ '' AS workTelephone,'GI' AS recordType
 
 from dbo.TLn04IntGntor
 	inner join dbo.TCl01Client on dbo.TLn04IntGntor.ClientId=dbo.TCl01Client.ID
@@ -88,10 +82,10 @@ from dbo.TLn04IntGntor
 
 
 )
-insert into dbo.CRBGuarantorTable (accountNumber,companyRegNo,companyRegistrationDate,country,dateOfBirth,foreName1,foreName2,foreName3,guarantorType,homeTelephone,institutionName,
-mobileTelephone,nationalId,nationality,passportNumber,placeOfBirth,postalAddressNumber,postalCode,surName,telephone1,telephone2,telephone3,town,tradingName,workTelephone,recordType)
-SELECT accountNumber,companyRegNo,companyRegistrationDate,country,dateOfBirth,foreName1,foreName2,foreName3,guarantorType,homeTelephone,institutionName,
-mobileTelephone,nationalId,nationality,passportNumber,placeOfBirth,postalAddressNumber,postalCode,surName,telephone1,telephone2,telephone3,town,tradingName,workTelephone,recordType from ConsumerLoansGuarantor;
+insert into dbo.CRBGuarantorTable (accountNumber,country,dateOfBirth,foreName1,foreName2,foreName3,homeTelephone,
+mobileTelephone,nationalId,nationality,passportNumber,placeOfBirth,postalAddressNumber,postalCode,surName,town,workTelephone,recordType)
+SELECT accountNumber,country,dateOfBirth,foreName1,foreName2,foreName3,homeTelephone,
+mobileTelephone,nationalId,nationality,passportNumber,placeOfBirth,postalAddressNumber,postalCode,surName,town,workTelephone,recordType from ConsumerLoansGuarantor;
     
 END
 
